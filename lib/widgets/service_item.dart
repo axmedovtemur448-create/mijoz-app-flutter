7a239370c32c3119a6d57087e19b7d0c95593b55
axmedovtemur448-create <axmedovtemur448@gmail.com>
2026-07-07 23:18:26 +0500

    Add files via upload

--- lib/widgets/service_item.dart
+++ lib/widgets/service_item.dart
@@ -1,1 +1,45 @@
+import 'package:flutter/material.dart';
+import '../data/services.dart';
 
+class ServiceItemWidget extends StatelessWidget {
+  final ServiceInfo service;
+  final VoidCallback? onTap;
+
+  const ServiceItemWidget({super.key, required this.service, this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(14),
+          border: Border.all(color: const Color(0xFFEFEFEF)),
+        ),
+        padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 4),
+        child: Column(
+          children: [
+            Container(
+              width: 48,
+              height: 48,
+              decoration: BoxDecoration(
+                color: service.color.withOpacity(0.1),
+                shape: BoxShape.circle,
+              ),
+              child: Icon(service.icon, color: service.color, size: 26),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              service.name,
+              textAlign: TextAlign.center,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: const TextStyle(fontSize: 11, fontWeight: FontWeight.w500, color: Color(0xFF1B1B1B)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
